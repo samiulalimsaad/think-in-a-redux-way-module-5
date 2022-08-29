944414e9b06ea5d5ddae50244bf2f0fd35b0edc3
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-29 11:32:53 +0600

    `5.6` added

--- vanilla-rtk/redux-toolkit/app/store.js
+++ vanilla-rtk/redux-toolkit/app/store.js
@@ -3,12 +3,17 @@ const { counterReducer } = require("../features/counter/counterSlice");
 const {
     dynamicCounterReducer,
 } = require("../features/dynamicCounter/dynamicCounterSlice");
+const { createLogger } = require("redux-logger");
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         dynamicCounter: dynamicCounterReducer,
     },
+    middleware: (getDefaultMiddleware) => [
+        ...getDefaultMiddleware(),
+        createLogger(),
+    ],
 });
 
 module.exports = store;
